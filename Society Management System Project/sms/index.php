59d40d491769467bbd1ec2bed2e847eb8bb8359a
Mr.Patil <propatil@protonmail.com>
2021-10-12 09:46:47 +0530

    initial commit

---
+++ Society Management System Project/sms/index.php
@@ -0,0 +1,132 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		
+		<title>Society Management System !! Home Page</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">=
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/nice-select.css">
+		    <link rel="stylesheet" href="css/ion.rangeSlider.css" />
+		    <link rel="stylesheet" href="css/ion.rangeSlider.skinFlat.css" />
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/main.css">
+		</head>
+		<body>
+
+			<!-- Start Header Area -->
+			<header class="default-header">
+		
+
+				<nav class="navbar navbar-expand-lg  navbar-light bg-light">
+					<div class="container">
+						  <a class="navbar-brand" href="index.php">
+						  	<h3 style="color: red">Society Management System</h3>
+						  </a>
+						  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						    <span class="navbar-toggler-icon"></span>
+						  </button>
+
+						  <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
+						    <ul class="navbar-nav">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="admin/login.php">Admin</a></li>
+								<li><a href="user/login.php">User</a></li>
+								
+						    </ul>
+						  </div>						
+					</div>
+				</nav>
+
+
+			</header>
+			<!-- End Header Area -->
+
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row fullscreen align-items-center justify-content-center" style="height: 915px;">
+						<div class="banner-content col-lg-12 col-md-12">
+							<h1 class="text-uppercase">
+								Society Management System
+							</h1>
+						
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+
+		
+				
+			<!-- Start city Area -->
+			<section class="city-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-4 col-md-4 mb-10">
+						  <div class="content">
+						    <img class="content-image img-fluid d-block mx-auto" src="img/p1.jpg" alt="">
+						   
+						  </div>
+						</div>
+						<div class="col-lg-8 col-md-8 mb-10">
+							<div class="content">
+							   
+							  		 <img class="content-image img-fluid d-block mx-auto" src="img/p2.jpg" alt="">
+							     
+							    </a>
+						  	</div>
+							<div class="row city-bottom">
+								<div class="col-lg-6 col-md-6 mt-30">
+									<div class="content">
+									  
+									  		 <img class="content-image img-fluid d-block mx-auto" src="img/p3.jpg" alt="">
+									     
+									    </a>
+									</div>
+								</div>
+								<div class="col-lg-6 col-md-6 mt-30">
+									<div class="content">
+									   	 <img class="content-image img-fluid d-block mx-auto" src="img/p4.jpg" alt="">
+									   
+									</div>
+								</div>								
+							</div>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End city Area -->
+	
+
+			<!-- start footer Area -->		
+			<footer class="footer-area section-gap">
+				<div class="container">
+					
+					<div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						<h2 class="footer-text m-0" style="color: red">Society Management System </h2>
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					</div>
+				</div>
+			</footer>	
+			<!-- End footer Area -->		
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.nice-select.min.js"></script>
+			<script src="js/jquery.sticky.js"></script>
+			<script src="js/ion.rangeSlider.js"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>
+			<script src="js/main.js"></script>	
+		</body>
+	</html>
